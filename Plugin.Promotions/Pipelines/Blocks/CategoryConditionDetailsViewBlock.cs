46fff326c13bb4ade145753023b1c77a4a8e73ca
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2019-09-27 17:04:44 +0200

    WIP

--- Plugin.Promotions/Pipelines/Blocks/CategoryConditionDetailsViewBlock.cs
+++ Plugin.Promotions/Pipelines/Blocks/CategoryConditionDetailsViewBlock.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Plugin.Promotions.Pipelines.Blocks
+namespace Promethium.Plugin.Promotions.Pipelines.Blocks
 {
     public class CategoryConditionDetailsViewBlock : PipelineBlock<EntityView, EntityView, CommercePipelineExecutionContext>
     {
