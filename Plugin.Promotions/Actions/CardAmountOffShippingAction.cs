4a6d43dbdd45a4f71e54a79e9cd8641859912693
Vincent van der Laan <vincent.van.der.laan@avivasolutions.nl>
2019-10-04 15:48:14 +0200

    IC-31 Added Action (benefit) for disocunt on fulfillment costs

---
+++ Plugin.Promotions/Actions/CardAmountOffShippingAction.cs
@@ -0,0 +1,63 @@
+﻿using Promethium.Plugin.Promotions.Extensions;
+using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Plugin.Carts;
+using Sitecore.Commerce.Plugin.Fulfillment;
+using Sitecore.Framework.Rules;
+using System.Linq;
+
+namespace Promethium.Plugin.Promotions.Actions
+{
+    /// <summary>
+    /// A SiteCore Commerce condition for the qualification
+    /// "Get [specific amount] off the shipping cost"
+    /// </summary>
+    [EntityIdentifier("Promethium_" + nameof(CardAmountOffShippingAction))]
+    public class CardAmountOffShippingAction : ICartAction
+    {
+        public IRuleValue<decimal> Promethium_SpecificAmount { get; set; }
+
+        public void Execute(IRuleExecutionContext context)
+        {
+            var commerceContext = context.Fact<CommerceContext>();
+            var cart = commerceContext?.GetObject<Cart>();
+            if (cart == null || !cart.Lines.Any() || !cart.HasComponent<FulfillmentComponent>())
+            {
+                return;
+            }
+
+            var fulfillmentFee = GetFulfillmentFee(cart);
+            if (fulfillmentFee == 0)
+            {
+                return;
+            }
+
+            var amountOff = Promethium_SpecificAmount.Yield(context);
+
+            //If the amount off is higher then the total fulfillment fee then lower the amount off
+            if (amountOff > fulfillmentFee)
+            {
+                amountOff = fulfillmentFee;
+            }
+
+            amountOff = amountOff.ShouldRoundPriceCalc(commerceContext);
+
+            cart.Adjustments.AddCartLevelAwardedAdjustment(commerceContext, amountOff * -1, nameof(CardAmountOffShippingAction));
+
+            cart.GetComponent<MessagesComponent>().AddPromotionApplied(commerceContext, nameof(CardAmountOffShippingAction));
+        }
+
+        private static decimal GetFulfillmentFee(Cart cart)
+        {
+            if (cart.HasComponent<SplitFulfillmentComponent>())
+            {
+                return cart.Lines
+                    .Select(line => line.Adjustments.FirstOrDefault(a => a.Name.EqualsOrdinalIgnoreCase("FulfillmentFee")))
+                    .TakeWhile(lineFulfillment => lineFulfillment != null)
+                    .Sum(lineFulfillment => lineFulfillment.Adjustment.Amount);
+            }
+
+            var awardedAdjustment = cart.Adjustments.FirstOrDefault(a => a.Name.EqualsOrdinalIgnoreCase("FulfillmentFee"));
+            return awardedAdjustment?.Adjustment.Amount ?? 0;
+        }
+    }
+}
---
+++ Plugin.Promotions/Extensions/ActionExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Sitecore.Commerce.Core;
+using Sitecore.Commerce.Plugin.Carts;
+using Sitecore.Commerce.Plugin.Pricing;
+using System;
+using System.Collections.Generic;
+
+namespace Promethium.Plugin.Promotions.Extensions
+{
+    public static class ActionExtensions
+    {
+        public static decimal ShouldRoundPriceCalc(this decimal input, CommerceContext context)
+        {
+            if (context.GetPolicy<GlobalPricingPolicy>().ShouldRoundPriceCalc)
+            {
+                input = Math.Round(input,
+                    context.GetPolicy<GlobalPricingPolicy>().RoundDigits,
+                    context.GetPolicy<GlobalPricingPolicy>().MidPointRoundUp
+                        ? MidpointRounding.AwayFromZero
+                        : MidpointRounding.ToEven);
+            }
+
+            return input;
+        }
+
+        public static void AddCartLevelAwardedAdjustment(this IList<AwardedAdjustment> adjustments, CommerceContext context, decimal amountOff, string awardingBlock)
+        {
+            var propertiesModel = context.GetObject<PropertiesModel>();
+            var discount = context.GetPolicy<KnownCartAdjustmentTypesPolicy>().Discount;
+
+            var adjustment = new CartLevelAwardedAdjustment
+            {
+                Name = propertiesModel?.GetPropertyValue("PromotionText") as string ?? discount,
+                DisplayName = propertiesModel?.GetPropertyValue("PromotionCartText") as string ?? discount,
+                Adjustment = new Money(context.CurrentCurrency(), amountOff),
+                AdjustmentType = discount,
+                IsTaxable = false,
+                AwardingBlock = awardingBlock
+            };
+
+            adjustments.Add(adjustment);
+        }
+
+        public static void AddPromotionApplied(this MessagesComponent messageComponent, CommerceContext context, string awardingBlock)
+        {
+            var propertiesModel = context.GetObject<PropertiesModel>();
+            var promotionName = propertiesModel?.GetPropertyValue("PromotionId") ?? awardingBlock;
+            messageComponent.AddMessage(context.GetPolicy<KnownMessageCodePolicy>().Promotions, $"PromotionApplied: {promotionName}");
+        }
+    }
+}
